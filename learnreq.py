cc1947e27e63c269ad774478a3088719800bfe26
Prabhashanjana De Alwis <prabhashanjanad@gmail.com>
2026-03-13 15:13:57 +0530

    add funtion for learn binary response content

--- learnreq.py
+++ learnreq.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 
 def Passing_parameters_in_URL():
@@ -13,4 +14,27 @@ def Passing_parameters_in_URL():
     print(f"Created_URL: {responce_1.url}")
 
 
+start_time = time.perf_counter()
 Passing_parameters_in_URL()
+end_time = time.perf_counter()
+
+print(f"Execution Time: {end_time - start_time:.4f} seconds")
+
+
+def binary_response():
+    print("Lession_two: Binary Response Content")
+    url_2 = "https://httpbin.org/image/png"
+
+    try:
+        responce_2 = requests.get(url_2)
+        if responce_2.status_code == 200:
+            with open("test_image.png", "wb") as file:
+                file.write(responce_2.content)
+
+            print("Image downloaded successfully.")
+
+    except Exception as e:
+        print(f"An error occurres: {e}")
+
+
+binary_response()
