cd9c64312ed53134dc48ccd6ab77af74f200cc9b
Prabhashanjana De Alwis <prabhashanjanad@gmail.com>
2026-03-13 23:38:16 +0530

    feat: add raw data, text content, and custom headers lessons

--- learnreq.py
+++ learnreq.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import shutil
 
 
 def Passing_parameters_in_URL():
@@ -15,10 +16,10 @@ def Passing_parameters_in_URL():
 
 
 start_time = time.perf_counter()
-Passing_parameters_in_URL()
+# Passing_parameters_in_URL()
 end_time = time.perf_counter()
 
-print(f"Execution Time: {end_time - start_time:.4f} seconds")
+# print(f"Execution Time: {end_time - start_time:.4f} seconds")
 
 
 def binary_response():
@@ -37,7 +38,7 @@ def binary_response():
         print(f"An error occurres: {e}")
 
 
-binary_response()
+# binary_response()
 
 
 def json_response():
@@ -55,4 +56,63 @@ def json_response():
         print(f"An error occurres: {e}")
 
 
-json_response()
+# json_response()
+
+
+def raw_response():
+    print("Lession_four: raw content response")
+    url_4 = "https://httpbin.org/image/png"
+    try:
+        response_4 = requests.get(url_4, stream=True)
+        if response_4.status_code == 200:
+
+            with open("raw_image.png", "wb") as file:
+                response_4.raw.decode_content = True
+                shutil.copyfileobj(response_4.raw, file)
+            print("Image downloaded successfully.")
+
+            print("First 10 raw byte:", response_4.raw.read(10))
+
+    except Exception as e:
+        print(f"An error occurres: {e}")
+
+
+# raw_response()
+
+
+def response_content():
+    print("Lession_five: Response Content")
+    url_5 = "https://httpbin.org/get"
+
+    try:
+        response_5 = requests.get(url_5)
+        if response_5.status_code == 200:
+            print(response_5.text[:200])
+
+    except Exception as e:
+        print(f"An error occurres: {e}")
+
+
+# response_content()
+
+
+def custom_headers():
+    print("Lession_six: custom headers")
+    url_6 = "https://httpbin.org/headers"
+
+    security_badge = {"Authorization": "Bearer TAPS_SUPER_SECRET_KEY_999"}
+    respose_6 = requests.get(url_6, headers=security_badge)
+
+    try:
+        if respose_6.status_code == 200:
+            print("✅ Access Granted! The security guard saw these headers:")
+            server_data = respose_6.json()
+            print(server_data["headers"])
+        else:
+            print("❌ Access Denied.")
+
+    except Exception as e:
+        print(f"An error occurres: {e}")
+
+
+custom_headers()
